045c97cdaa949422da3df45bc3745c5f3f71e09a
eteva <etevans2327@gmail.com>
2025-05-02 18:37:21 -0500

    Added variables, constructor, getters, and setters to ExoticAnimal

--- src/main/java/petadoption/model/ExoticAnimal.java
+++ src/main/java/petadoption/model/ExoticAnimal.java
@@ -1,5 +1,72 @@
 package petadoption.model;
 
 public class ExoticAnimal {
+	
+	private String uniqueId;
+    private String animalName;
+    private String category;
+    private String subSpecies;
+    private int yearsOld;
+    private boolean adopted;
+    
+    public ExoticAnimal(String uniqueId, String animalName, String category, String subSpecies, int yearsOld) {
+		super();
+		this.uniqueId = uniqueId;
+		this.animalName = animalName;
+		this.category = category;
+		this.subSpecies = subSpecies;
+		this.yearsOld = yearsOld;
+		this.adopted = false;
+	}
+
+	public String getUniqueId() {
+		return uniqueId;
+	}
+
+	public void setUniqueId(String uniqueId) {
+		this.uniqueId = uniqueId;
+	}
+
+	public String getAnimalName() {
+		return animalName;
+	}
+
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getSubSpecies() {
+		return subSpecies;
+	}
+
+	public void setSubSpecies(String subSpecies) {
+		this.subSpecies = subSpecies;
+	}
+
+	public int getYearsOld() {
+		return yearsOld;
+	}
+
+	public void setYearsOld(int yearsOld) {
+		this.yearsOld = yearsOld;
+	}
+
+	public boolean isAdopted() {
+		return adopted;
+	}
+
+	public void setAdopted(boolean adopted) {
+		this.adopted = adopted;
+	}
+    
+    
 
 }
