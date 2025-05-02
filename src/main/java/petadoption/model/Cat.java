a7dbb87636e21df6541ec2de04bb48f5bfbfd3fa
eteva <etevans2327@gmail.com>
2025-05-02 18:30:39 -0500

    Added constructor to Cat

--- src/main/java/petadoption/model/Cat.java
+++ src/main/java/petadoption/model/Cat.java
@@ -1,5 +1,9 @@
 package petadoption.model;
 
-public class Cat {
+public class Cat extends Pet{
+
+	public Cat(int id, String name, String species, int age) {
+		super(id, name, "Cat", species, age);
+	}
 
 }
