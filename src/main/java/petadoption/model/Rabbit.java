3599d0412f02f5b462ff9fd5d5d500d52111f84d
eteva <etevans2327@gmail.com>
2025-05-02 18:33:09 -0500

    Added constructor to Rabbit

--- src/main/java/petadoption/model/Rabbit.java
+++ src/main/java/petadoption/model/Rabbit.java
@@ -1,5 +1,9 @@
 package petadoption.model;
 
-public class Rabbit {
+public class Rabbit extends Pet{
+
+	public Rabbit(int id, String name, String species, int age) {
+		super(id, name, "Rabbit", species, age);
+	}
 
 }
