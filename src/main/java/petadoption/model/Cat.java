3b168230563de082eaf2f7ed1dfe7ac2932df0f8
eteva <etevans2327@gmail.com>
2025-05-02 19:48:25 -0500

    Added JavaDoc to Cat

--- src/main/java/petadoption/model/Cat.java
+++ src/main/java/petadoption/model/Cat.java
@@ -2,6 +2,14 @@ package petadoption.model;
 
 public class Cat extends Pet{
 
+	/**
+     * Constructs a new Cat object with the specified id, name, species, and age.
+     * 
+     * @param id the unique id for the cat
+     * @param name the name of the cat
+     * @param species the species of the cat
+     * @param age the age of the cat
+     */
 	public Cat(int id, String name, String species, int age) {
 		super(id, name, "Cat", species, age);
 	}
