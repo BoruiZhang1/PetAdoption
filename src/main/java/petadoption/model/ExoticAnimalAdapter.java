a3111a2795a6749e5540db9e388881af6a4903d2
Borui Zhang <borui@botenders.com>
2025-05-03 14:03:06 -0500

    ExoticAnimalAdaptor skeleton

--- src/main/java/petadoption/model/ExoticAnimalAdapter.java
+++ src/main/java/petadoption/model/ExoticAnimalAdapter.java
@@ -1,5 +1,22 @@
 package petadoption.model;
 
-public class ExoticAnimalAdapter {
+public class ExoticAnimalAdapter extends Pet {
 
+	private ExoticAnimal exoticAnimal;
+	
+	public ExoticAnimalAdapter(ExoticAnimal exoticAnimal) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExoticAnimal getExoticAnimal() {
+		return exoticAnimal;
+	}
+
+	public void setExoticAnimal(ExoticAnimal exoticAnimal) {
+		this.exoticAnimal = exoticAnimal;
+	}
+	
+	
+	
+	
 }
