97cc09bffc5f0230ddb886cdeb0d462f8bfe74ba
Borui Zhang <borui@botenders.com>
2025-05-03 17:10:02 -0500

    Skeleton for save and load Datamanager

--- src/main/java/petadoption/data/PetDataManager.java
+++ src/main/java/petadoption/data/PetDataManager.java
@@ -1,5 +1,33 @@
 package petadoption.data;
 
-public class PetDataManager {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import petadoption.model.Pet;
+import petadoption.model.Shelter;
+
+import java.io.*;
+import java.util.*;
+
+import org.ietf.jgss.GSSException;
+
+public class PetDataManager {
+	
+		private Gson gson;
+		
+		public PetDataManager()
+		{
+			this.gson = new Gson();
+		}
+	
+		public Shelter<Pet> loadPetData() throws IOException
+		{
+			return null;
+			
+		}
+	
+		public Shelter<Pet> savePetData() throws IOException
+		{
+			
+		}
 }
