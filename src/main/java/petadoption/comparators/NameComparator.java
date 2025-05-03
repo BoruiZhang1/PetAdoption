332d212223ed832d7345e4032406174590e57587
eteva <etevans2327@gmail.com>
2025-05-02 19:15:33 -0500

    Added compare method to NameComparator

--- src/main/java/petadoption/comparators/NameComparator.java
+++ src/main/java/petadoption/comparators/NameComparator.java
@@ -1,5 +1,14 @@
 package petadoption.comparators;
 
-public class NameComparator {
+import java.util.Comparator;
+
+import petadoption.model.Pet;
+
+public class NameComparator implements Comparator<Pet>{
+
+	@Override
+	public int compare(Pet p1, Pet p2) {
+		return p1.getName().compareToIgnoreCase(p2.getName());
+	}
 
 }
