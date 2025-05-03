5ff01accde0b721af6d15656be55ba823d38a162
eteva <etevans2327@gmail.com>
2025-05-02 19:49:29 -0500

    Added JavaDoc for Dog

--- src/main/java/petadoption/model/Dog.java
+++ src/main/java/petadoption/model/Dog.java
@@ -2,6 +2,14 @@ package petadoption.model;
 
 public class Dog extends Pet{
 
+	/**
+     * Constructs a new Dog object with the specified id, name, species, and age.
+     * 
+     * @param id the unique id for the dog
+     * @param name the name of the dog
+     * @param species the species of the dog
+     * @param age the age of the dog
+     */
 	public Dog(int id, String name, String species, int age) {
 		super(id, name, "Dog", species, age);
 	}
