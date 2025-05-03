480eb5b451434da4dc63c6225cc172f838cf7fc1
eteva <etevans2327@gmail.com>
2025-05-02 19:50:12 -0500

    Added JavaDoc for Rabbit

--- src/main/java/petadoption/model/Rabbit.java
+++ src/main/java/petadoption/model/Rabbit.java
@@ -2,6 +2,14 @@ package petadoption.model;
 
 public class Rabbit extends Pet{
 
+	/**
+     * Constructs a new Rabbit object with the specified id, name, species, and age.
+     * 
+     * @param id the unique id for the rabbit
+     * @param name the name of the rabbit
+     * @param species the species of the rabbit
+     * @param age the age of the rabbit
+     */
 	public Rabbit(int id, String name, String species, int age) {
 		super(id, name, "Rabbit", species, age);
 	}
