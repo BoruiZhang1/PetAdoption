d04b4023465bbfd7bea4fc6642a0562c54794a32
eteva <etevans2327@gmail.com>
2025-05-02 19:18:55 -0500

    Added compare to SpeciesComparator

--- src/main/java/petadoption/comparators/SpeciesComparator.java
+++ src/main/java/petadoption/comparators/SpeciesComparator.java
@@ -1,5 +1,14 @@
 package petadoption.comparators;
 
-public class SpeciesComparator {
+import java.util.Comparator;
+
+import petadoption.model.Pet;
+
+public class SpeciesComparator implements Comparator<Pet>{
+
+	@Override
+	public int compare(Pet p1, Pet p2) {
+		return p1.getSpecies().compareToIgnoreCase(p2.getSpecies());
+	}
 
 }
