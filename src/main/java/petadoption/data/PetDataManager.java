3ab2ca1e718e76b9d7ef2bca8c8819cd4add4479
Borui Zhang <borui@botenders.com>
2025-05-03 18:19:17 -0500

    Helper functions for loading normal or exotic pets

--- src/main/java/petadoption/data/PetDataManager.java
+++ src/main/java/petadoption/data/PetDataManager.java
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken;
 
 import petadoption.model.Pet;
 import petadoption.model.Shelter;
+import petadoption.model.ExoticAnimal;
 
 import java.io.*;
 import java.util.*;
@@ -22,12 +23,30 @@ public class PetDataManager {
 	
 		public Shelter<Pet> loadPetData() throws IOException
 		{
-			return null;
+			Shelter<Pet> shelter = new Shelter<Pet>();
+			
 			
 		}
 	
 		public Shelter<Pet> savePetData() throws IOException
 		{
+			
+		}
+		
+		private List<Pet> loadNormal() throws IOException
+		{
+			List<Pet> pets = new ArrayList<Pet>();
+			String path = getClass().getResource("/pets.json").getFile();
+			try
+			{
+				BufferedReader br = new BufferedReader(new FileReader(path));
+				List<>
+			}
+		}
+		
+		private List<ExoticAnimal> loadExotic() throws IOException
+		{
+			return null;
 			
 		}
 }
