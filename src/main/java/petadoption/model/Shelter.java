f2ecf3e21eaa7d9d3c2af2ab95da7fded56d1793
eteva <etevans2327@gmail.com>
2025-05-02 19:20:53 -0500

    Added pets List, constructors, addPet, and removePet method to shelter

--- src/main/java/petadoption/model/Shelter.java
+++ src/main/java/petadoption/model/Shelter.java
@@ -1,5 +1,26 @@
 package petadoption.model;
 
-public class Shelter {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Shelter<T extends Pet> {
+
+private List<T> pets;
+    
+    public Shelter() {
+        this.pets = new ArrayList<>();
+    }
+    
+    public Shelter(List<T> pets) {
+        this.pets = new ArrayList<>(pets);
+    }
+
+    void addPet(T pet) {
+        pets.add(pet);
+    }
+    
+    public boolean removePet(T pet) {
+        return pets.remove(pet);
+    }
+    
 }
