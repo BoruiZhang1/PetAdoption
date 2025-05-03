4da07bffc666aea4c0a70db5affcda12dd6a45e2
eteva <etevans2327@gmail.com>
2025-05-02 19:17:22 -0500

    Added compare method to AgeComparator

--- src/main/java/petadoption/comparators/AgeComparator.java
+++ src/main/java/petadoption/comparators/AgeComparator.java
@@ -1,5 +1,14 @@
 package petadoption.comparators;
 
-public class AgeComparator {
+import java.util.Comparator;
 
+import petadoption.model.Pet;
+
+public class AgeComparator implements Comparator<Pet>{
+
+	@Override
+	public int compare(Pet p1, Pet p2) {
+		return Integer.compare(p1.getAge(), p2.getAge());
+	}
+	
 }
