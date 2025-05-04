8852a448f9dac2f127b3c6fabbfaf215529df18b
Hao Ouyang <joe520088@gmail.com>
2025-05-04 01:34:08 -0500

    set title for the site, create name label and the type of animal.

--- src/main/java/petadoption/view/AddPetDialog.java
+++ src/main/java/petadoption/view/AddPetDialog.java
@@ -6,6 +6,7 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
@@ -39,28 +40,24 @@ public class AddPetDialog extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public AddPetDialog() {
-		setBounds(100, 100, 450, 300);
+	public AddPetDialog() 
+	{
+		setTitle("Add New Pet");
+		setBounds(100, 100, 450, 250);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+		
+		// create Labels 
+		JLabel nameLabel = new JLabel("Name:");
+		nameField = new JTextField(20);
+		
+		JLabel typeLabel = new JLabel("Type:");
+		String[] petTypes = {"Dog", "Cat", "Rabbit", "Other"};
+		typeComboBox = new JComboBox<>(petTypes);
+		
+		
 	}
 
 }
