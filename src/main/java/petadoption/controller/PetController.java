4baf789615acffe7a556573e5cb26d19611701af
Borui Zhang <borui@botenders.com>
2025-05-04 11:22:41 -0500

    Controller skeleton
    field setup and empty constructor.

--- src/main/java/petadoption/controller/PetController.java
+++ src/main/java/petadoption/controller/PetController.java
@@ -1,5 +1,21 @@
 package petadoption.controller;
 
-public class PetController {
+import petadoption.data.PetDataManager;
+import petadoption.model.Pet;
+import petadoption.model.Shelter;
+import petadoption.view.MainFrame;
 
+public class PetController {
+	
+	
+	private MainFrame landing;
+	private Shelter<Pet> shelter;
+	private PetDataManager manager;
+	
+	public PetController() 
+	{
+		
+	}
+	
+	
 }
