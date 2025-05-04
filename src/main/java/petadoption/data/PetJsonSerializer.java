b93c655c99c4b4f0282b7badb2d9692d64b8d1bf
Borui Zhang <borui@botenders.com>
2025-05-03 20:46:35 -0500

    jsonToPets returns the list of pet object after reading the json stuff

--- src/main/java/petadoption/data/PetJsonSerializer.java
+++ src/main/java/petadoption/data/PetJsonSerializer.java
@@ -1,5 +1,61 @@
 package petadoption.data;
 
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import petadoption.data.PetDTO.PetJson;
+import petadoption.model.Cat;
+import petadoption.model.Dog;
+import petadoption.model.Pet;
+import petadoption.model.Rabbit;
+
 public class PetJsonSerializer {
 
+	private Gson gson = new Gson();
+	
+	public List<Pet> jsonToPets(Reader reader)
+	{
+		Type listType = new TypeToken<ArrayList<PetJson>>(){}.getType();
+		List<PetJson> petJsonList = gson.fromJson(reader, listType);
+		
+		List<Pet> pets = new ArrayList<>();
+		for (PetJson json : petJsonList) 
+		{
+			Pet pet = null; 
+			if ("Dog".equals(json.type))
+			{
+				pet = new Dog(json.id, json.name, json.species, json.age);
+			}
+			else if ("Cat".equals(json.type))
+			{
+				pet = new Cat(json.id, json.name, json.species, json.age);
+			}
+			else if ("Rabbit".equals(json.type))
+			{
+				pet = new Rabbit(json.id, json.name, json.species, json.age);
+			}
+			
+			if(pet != null)
+			{
+				if(json.adopted)
+				{
+					pet.setAdopted(true);
+				}
+				pets.add(pet);
+			}	
+		}
+		return pets;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
