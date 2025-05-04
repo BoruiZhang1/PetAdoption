1eceb5ba0e5ebfd2511e151d4a9a708c35642fdc
Hao Ouyang <joe520088@gmail.com>
2025-05-03 19:39:41 -0500

    setting up petDialog window

--- src/main/java/petadoption/view/AddPetDialog.java
+++ src/main/java/petadoption/view/AddPetDialog.java
@@ -1,5 +1,42 @@
 package petadoption.view;
 
-public class AddPetDialog {
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AddPetDialog extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddPetDialog frame = new AddPetDialog();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AddPetDialog() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
 
 }
